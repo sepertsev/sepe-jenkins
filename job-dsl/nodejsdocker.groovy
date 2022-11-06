87c26f76e8b9d4bf19095165e48eb22b2097dd32
sepertsev <117574098+sepertsev@users.noreply.github.com>
2022-11-06 14:47:52 +0300

    Change up to sepertsev repo creds

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('spertsev/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('sepe_dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
